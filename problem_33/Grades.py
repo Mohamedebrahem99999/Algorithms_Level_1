7b4a5698b8ca56fce1f84a83b7fcfd3c235421b8
Mohamed Ebrahim <jshdheh33i38@gmail.com>
2026-03-13 07:54:10 +0200

    Add python implementation for the student grades problem

---
+++ problem_33/Grades.py
@@ -0,0 +1,13 @@
+degree = float(input("Enter your grade?\n"))
+if degree >= 90:
+  print("A")
+if degree >= 80:
+  print("B")
+elif degree >= 70:
+  print("C")
+elif degree >= 60:
+  print("D")
+elif degree >= 50:
+  print("E")
+else:
+  print("F") 
