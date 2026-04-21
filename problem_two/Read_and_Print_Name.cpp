e8107e90f9b9401158a4cb20cc459a93fe339917
Mohamed Ebrahim <jshdheh33i38@gmail.com>
2026-04-21 23:45:39 +0200

    Organize the code into functions

--- problem_two/Read_and_Print_Name.cpp
+++ problem_two/Read_and_Print_Name.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <string>
-
+std::string get_full_name(std::string first_name, std::string last_name)
+{
+    return first_name +" "+ last_name;
+}
 std::string readName()
 {
     std::cout << "Enter your name\n";
-    std::string Name;
-    std::getline(std::cin, Name);
-    return Name;
+    std::string first_name, last_name, full_name;
+    std::cin >> first_name >> last_name;
+    return get_full_name(first_name, last_name);
 }
 
 void printName(std::string Name)
