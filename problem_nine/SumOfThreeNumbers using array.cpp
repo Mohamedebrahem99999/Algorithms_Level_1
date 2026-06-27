53ec16f30086d919ad6949e810b69703b9b2e3de
Mohamed Ebrahim <jshdheh33i38@gmail.com>
2026-06-27 17:39:57 +0300

    Add problem nine solution code

--- problem_nine/SumOfThreeNumbers using array.cpp
+++ problem_nine/SumOfThreeNumbers using array.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+void readNums(int arr[3])
+{
+    std::cout << "Enter the furst number\n";
+    std::cin >> arr[0];
+    std::cout << "Enter the second number\n";
+    std::cin >> arr[1];
+    std::cout << "Enter the third number\n";
+    std::cin >> arr[2];
+}
+
 int SumOfThreeNumbers(int Num1, int Num2, int Num3)
 {
     return Num1 + Num2 + Num3;
@@ -13,12 +23,6 @@ void PrintResult(int Total)
 int main()
 {
     int arr[3];
-    std::cout << "Enter the furst number\n";
-    std::cin >> arr[0];
-    std::cout << "Enter the second number\n";
-    std::cin >> arr[1];
-    std::cout << "Enter the third number\n";
-    std::cin >> arr[2];
-    
-    PrintResult(SumOfThreeNumbers(arr[0],arr[1], arr[2]));
+    readNums(arr);
+    PrintResult(SumOfThreeNumbers(arr[0], arr[1], arr[2]));
 }
