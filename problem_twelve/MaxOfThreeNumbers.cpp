4420b84efcdae2c77b8fd7279f2de43e2e5938a4
Mohamed Ebrahim <jshdheh33i38@gmail.com>
2026-06-27 05:20:05 +0300

    Add problem 12 solution code

---
+++ problem_twelve/MaxOfThreeNumbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+void readNumbers(int &Num1, int &Num2)
+{
+    std::cout << "Enter the first Number \n";
+    std::cin >> Num1;
+    std::cout << "Enter the second Number \n";
+    std::cin >> Num2;
+}
+
+int setMax(int Num1, int Num2)
+{
+    return (Num1 >= Num2? Num1: Num2) ;
+}
+
+void printMax(int Max)
+{
+    std::cout << Max << " is the Maximum\n";
+}
+
+int main()
+{
+    int Num1, Num2;
+    readNumbers(Num1,Num2);
+    printMax(setMax(Num1,Num2));
+
+    
+
+}
