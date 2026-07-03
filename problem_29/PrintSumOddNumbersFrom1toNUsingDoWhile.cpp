6c517ec3d78647a31976516328e9e74b94afca84
Mohamed Ebrahim <jshdheh33i38@gmail.com>
2026-07-03 17:56:36 +0300

    Add problem 29 solution code using Do While Loop

---
+++ problem_29/PrintSumOddNumbersFrom1toNUsingDoWhile.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+enum enOddOrEven {Odd, Even};
+
+int readNumber()
+{
+    int Number;
+    std::cout << "Enter the End\n";
+    std::cin >> Number;
+    return Number;
+}
+
+enOddOrEven isOdd(int Number)
+{
+    return (Number %2 != 0 ? enOddOrEven::Odd :enOddOrEven::Even);
+}
+
+int SumOfOddNums(int End)
+{
+    int Sum = 0;
+    int i = 1;
+    do {    
+        Sum+=(isOdd(i) == enOddOrEven::Even?i:0);
+        i++;
+    }
+    while (i <= End);
+    
+    return Sum;
+}
+
+void printSum(int Sum)
+{
+    std::cout << "The total Sum of the numbers is " << Sum << std::endl;
+}
+
+int main()
+{
+    printSum(SumOfOddNums(readNumber()));
+}
