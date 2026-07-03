42a8eaa09c6cfaea4883603856c394168b1500a3
Mohamed Ebrahim <jshdheh33i38@gmail.com>
2026-07-03 17:29:23 +0300

    Add problem 28 solution code (Using For Loop)

---
+++ problem_28/PrintSumOddNumbersFrom1toNUsingFor.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+enum enOddOrEven {Odd, Even};
+
+int readNumber()
+{
+    int Number;
+    std::cout << "Enter the End\n";
+    std::cin >> Number;
+    return Number;
+}
+
+enOddOrEven isOdd(int Number)
+{
+    return (Number %2 != 0 ? enOddOrEven::Odd :enOddOrEven::Even);
+}
+
+int SumOfOddNums(int End)
+{
+    int Sum = 0;
+    for(int i = 1; i <= End;i++)
+    {    
+        Sum+=(isOdd(i) == enOddOrEven::Odd?i:0);
+    }
+    return Sum;
+}
+
+void printSum(int Sum)
+{
+    std::cout << "The total Sum of the numbers is " << Sum << std::endl;
+}
+
+int main()
+{
+    printSum(SumOfOddNums(readNumber()));
+}
